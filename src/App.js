6a31950e1e7f7578c2c37adf06f01a80fcbc4833
ninypops <ninypopsgame@gmail.com>
2023-01-10 17:15:20 +0000

    WIP Remove unnecessary code and files from project, install dependencies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import main gallery component here
+
+function App() {
+  return (
+    <React.Fragment>
+      <p>Test</p>
+      {/* Main component rendered */}
+    </React.Fragment>
+  );
+}
+
+export default App;
